0edccf11bdf2e7cfdce84b08e86f3311dff62a5a
Sanam Malik <sanam.malik04@gmail.com>
2020-06-24 17:31:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,12 @@ def loop_message_n_times(message, limit)
     count += 1
   end
 end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+
