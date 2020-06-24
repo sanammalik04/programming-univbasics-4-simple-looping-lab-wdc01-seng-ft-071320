99bcab58d6379e2d65678158a08f8254f92f9754
Sanam Malik <sanam.malik04@gmail.com>
2020-06-24 17:31:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -48,3 +48,12 @@ def return_string_array(array)
   new_array
 end
 
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
+
